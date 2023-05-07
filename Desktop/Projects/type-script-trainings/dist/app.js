c20bbe871e935335e23f94b0a88b55c6a55c7d59
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-05-07 17:43:07 +0300

    feat: project started, template engine has been used

--- Desktop/Projects/type-script-trainings/dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-const user = {
-    name: 'Alex',
-};
-console.log((_a = user === null || user === void 0 ? void 0 : user.additionInfo) === null || _a === void 0 ? void 0 : _a.someInfo);
-//# sourceMappingURL=app.js.map
