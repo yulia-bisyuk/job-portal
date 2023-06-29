b9bf125ff410c45618e13082e4f3058ebfa04012
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-06-29 20:28:59 +0300

    fix: git checks

--- jobs-portal/src/constants/index.js
+++ jobs-portal/src/constants/index.js
@@ -66,11 +66,11 @@ export const categories = [
 
 export const popularCategories = [];
 
-// export const levels = [
-//   { id: 'A0/A1', name: '(Beginner/Elementary)' },
-//   { id: 'A2', name: '(Pre Intermediate)' },
-//   { id: 'B1', name: '(Intermediate)' },
-//   { id: 'B2', name: '(Upper Intermediate)' },
-//   { id: 'C1', name: '(Advanced)' },
-//   { id: 'C2', name: '(Proficient)' },
-// ];
+export const levels = [
+  { id: 'A0/A1', name: '(Beginner/Elementary)' },
+  { id: 'A2', name: '(Pre Intermediate)' },
+  { id: 'B1', name: '(Intermediate)' },
+  { id: 'B2', name: '(Upper Intermediate)' },
+  { id: 'C1', name: '(Advanced)' },
+  { id: 'C2', name: '(Proficient)' },
+];
